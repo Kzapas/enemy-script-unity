4f48a478adfd374007d3a625e981b4adb985d39d
Kipras Vitas <33299760+Kzapas@users.noreply.github.com>
2020-05-03 17:32:04 -0400

    Add files via upload

---
+++ EnemyBorder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBorder : MonoBehaviour
+{
+
+    public GameObject Enemy;
+    public float Spotting_Distance_Enter;
+    public float Spotting_Distance_Exit;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Slender"))
+        {
+            Enemy.GetComponent<EnemyFollow>().Spotting_Distance = Spotting_Distance_Enter;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Slender"))
+        {
+            Enemy.GetComponent<EnemyFollow>().Spotting_Distance = Spotting_Distance_Exit;
+        }
+    }
+}
